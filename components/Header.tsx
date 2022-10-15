36b53cc4f40bec381edb062e7cd133790be0feb0
kevinokello <okellokevin77@gmail.com>
2022-10-15 11:35:21 +0300

    header links

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,20 +3,25 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
-  return (
-      <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
-          <div className="flex items-center justify-center md:w-1/5">
-              <Link href="/">
-                  <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
-                      <Image
-                          src="https://rb.gy/vsvv2o"
-                          layout="fill"
-                          objectFit="contain"
-                      />
-                  </div>
-              </Link>
-          </div>
-     
-      </header>
-  )
+    return (
+        <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
+            <div className="flex items-center justify-center md:w-1/5">
+                <Link href="/">
+                    <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+                        <Image
+                            src="https://rb.gy/vsvv2o"
+                            layout="fill"
+                            objectFit="contain"
+                        />
+                    </div>
+                </Link>
+            </div>
+            <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+                <a className="headerLink">Product</a>
+                <a className="headerLink">Explore</a>
+                <a className="headerLink">Support</a>
+                <a className="headerLink">Business</a>
+            </div>
+        </header>
+    )
 }
